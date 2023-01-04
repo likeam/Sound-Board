2cafbbf768ee033fb8922bc2712ad507b6810c9b
AbdulRehman <likeam99@gmail.com>
2023-01-04 18:03:49 +0500

    controller

---
+++ controller.js
@@ -0,0 +1,26 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+	const btn = document.createElement('button')
+	btn.classList.add('btn');
+	let l =  174*(sound.length);
+	console.log(l);
+	btn.innerText = sound
+	btn.style.backgroundColor = `#${l}`;
+
+	btn.addEventListener('click', ()=>{
+		document.getElementById(sound).play()
+	})
+
+	document.getElementById('buttons').appendChild(btn);
+})
+
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
